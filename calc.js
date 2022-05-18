4c65ae5e1e17bb8e2d64c0f1be29e06486edac7b
ARC-ATC <105395619+ARC-ATC@users.noreply.github.com>
2022-05-18 14:37:33 -0700

    Updated: Simplified, One function calculates all calculations depending on variable sent into function.

--- calc.js
+++ calc.js
@@ -3,6 +3,7 @@
   "use strict";
 
   /**
+   * Updated 5_18 by Sergey Akhromtsev/ ARC DSPS
    * The starting point in our program, setting up a listener
    * for the "load" event on the window, signalling the HTML DOM has been constructed
    * on the page. When this event occurs, the attached function (init) will be called.
@@ -38,26 +39,34 @@
     var text2Value = text2.value;
     var calcType = document.getElementById("calc-type");
 
+    // if(calcType.value == "oneX"){
+    //   let output = oneF(textValue, text2Value);
+    //   outText(output);
+    // }
     if(calcType.value == "oneX"){
-      let output = oneF(textValue, text2Value);
+      let output = calcF(textValue, text2Value, 1.5);
       outText(output);
     }
     else if(calcType.value == "twoX"){
-      let output = twoF(textValue, text2Value);
+      let output = calcF(textValue, text2Value, 2.0);
       outText(output);
     }
     else if(calcType.value == "threeX"){
-      let output = threeF(textValue, text2Value);
+      let output = calcF(textValue, text2Value, 2.5);
       outText(output);
     }
     else if(calcType.value == "fourX"){
-      let output = fourF(textValue, text2Value);
+      let output = calcF(textValue, text2Value, 3.0);
       outText(output);
     }
     /*console.log(output);*/
   }
 
-  function oneF(orig, sTime){
+
+  /*
+  ** Mult sends the multiplier, orig is the original exam length in class, sTime is exam start time at DSPS
+  */
+  function calcF(orig, sTime, mult){
   	let flag = true; //true am
     orig = orig.split(":");
     sTime = sTime.split(":");
@@ -85,7 +94,7 @@
     }
 
     var seconds = orig[0] * 60 * 60 + orig[1] * 60;//original exam time in seconds
-    seconds = seconds* (1.5);//original exam time multiplied by multiplier
+    seconds = seconds* (mult);//original exam time multiplied by multiplier
 
     var origMultHrs = Math.floor((seconds/60)/60);
     var origMultMns = Math.floor((seconds - ((origMultHrs*60)*60))/60);
@@ -133,236 +142,7 @@
     " ] Exam Stop Time: [ "+ hrs.toString().padStart(2, '0') + ":" +  NewMns +" ]";
   }
 
-  function twoF(orig, sTime){
-  	let flag = true; //true am
-    orig = orig.split(":");
-    sTime = sTime.split(":");
-    var origDate = new Date(0, 0, 0, orig[0], orig[1], 0, 0);
-    var startDate = new Date(0, 0, 0, sTime[0], sTime[1], 0, 0);
-
-    // minutes are worth 60 seconds. Hours are worth 60 minutes.
-    if(orig[0]>12){
-      orig[0] = orig[0]-12;//keeping within 12 hr format
-    }
-    if(sTime[0]>=12){
-    	if(sTime[0]<24){
-    		flag = false; //pm
-    	}
-    	else if(sTime[0]===12){
-    		flag=false;
-    	}
-    	else{
-    		flag = true; //am
-    	}
-      if(sTime[0]!=12)
-      {
-      	sTime[0] = sTime[0]-12;//keeping within 12 hr format if hr between 13 and 24 hrs
-      }
-    }
-
-    var seconds = orig[0] * 60 * 60 + orig[1] * 60;//original exam time in seconds
-    seconds = seconds* (2);//original exam time multiplied by multiplier
-
-    var origMultHrs = Math.floor((seconds/60)/60);
-    var origMultMns = Math.floor((seconds - ((origMultHrs*60)*60))/60);
-    var secb = sTime[0] * 60 * 60 + sTime[1] * 60;// start time
-    var res = seconds + secb;
-
-    var hrs = Math.floor((res/60)/60);
-    var mns = Math.floor((res - ((hrs*60)*60))/60);
-    if(hrs>12){
-      hrs = hrs-12;
-    }
-
-    var NewMns = mns.toString().padStart(2, '0');
-    if(flag){
-    	if((hrs=== 12 || hrs<sTime[0])&& sTime[0]!=12){
-    		NewMns = NewMns + "PM";
-    	}
-    	else if(hrs>sTime[0]){
-    		NewMns = NewMns + "AM";
-    	}
-    	else if(sTime[0]===12 && hrs<sTime[0]){
-    		NewMns = NewMns + "AM";
-    	}
-    	else{
-    		NewMns = NewMns + "AM";
-    	}
-    }
-    else if(!flag){
-    	if((hrs=== 12 || hrs<sTime[0])&& sTime[0]!=12){
-    		NewMns = NewMns + "AM";
-    	}
-    	else if(hrs>sTime[0]){
-    		NewMns = NewMns + "PM";
-    	}
-    	else if(sTime[0]===12 && hrs<sTime[0]){
-    		NewMns = NewMns + "PM";
-    	}
-    	else{
-    		NewMns = NewMns + "PM";
-    	}
-    }
-
-    console.log(hrs + ":" + mns);
-    return "Multiplied Exam Time: [ " + origMultHrs.toString().padStart(2, '0') +  ":" + origMultMns.toString().padStart(2, '0') +
-    " ] Exam Stop Time: [ "+ hrs.toString().padStart(2, '0') + ":" +  NewMns +" ]";
-  }
-
-  function threeF(orig, sTime){
-	let flag = true; //true am
-    orig = orig.split(":");
-    sTime = sTime.split(":");
-    var origDate = new Date(0, 0, 0, orig[0], orig[1], 0, 0);
-    var startDate = new Date(0, 0, 0, sTime[0], sTime[1], 0, 0);
-
-    // minutes are worth 60 seconds. Hours are worth 60 minutes.
-    if(orig[0]>12){
-      orig[0] = orig[0]-12;//keeping within 12 hr format
-    }
-    if(sTime[0]>=12){
-    	if(sTime[0]<24){
-    		flag = false; //pm
-    	}
-    	else if(sTime[0]===12){
-    		flag=false;
-    	}
-    	else{
-    		flag = true; //am
-    	}
-      if(sTime[0]!=12)
-      {
-      	sTime[0] = sTime[0]-12;//keeping within 12 hr format if hr between 13 and 24 hrs
-      }
-    }
-
-    var seconds = orig[0] * 60 * 60 + orig[1] * 60;//original exam time in seconds
-    seconds = seconds* (2.5);//original exam time multiplied by multiplier
-
-    var origMultHrs = Math.floor((seconds/60)/60);
-    var origMultMns = Math.floor((seconds - ((origMultHrs*60)*60))/60);
-    var secb = sTime[0] * 60 * 60 + sTime[1] * 60;// start time
-    var res = seconds + secb;
-
-    var hrs = Math.floor((res/60)/60);
-    var mns = Math.floor((res - ((hrs*60)*60))/60);
-    if(hrs>12){
-      hrs = hrs-12;
-    }
-
-   var NewMns = mns.toString().padStart(2, '0');
-   if(flag){
-    	if((hrs=== 12 || hrs<sTime[0])&& sTime[0]!=12){
-    		NewMns = NewMns + "PM";
-    	}
-    	else if(hrs>sTime[0]){
-    		NewMns = NewMns + "AM";
-    	}
-    	else if(sTime[0]===12 && hrs<sTime[0]){
-    		NewMns = NewMns + "AM";
-    	}
-    	else{
-    		NewMns = NewMns + "AM";
-    	}
-    }
-    else if(!flag){
-    	if((hrs=== 12 || hrs<sTime[0])&& sTime[0]!=12){
-    		NewMns = NewMns + "AM";
-    	}
-    	else if(hrs>sTime[0]){
-    		NewMns = NewMns + "PM";
-    	}
-    	else if(sTime[0]===12 && hrs<sTime[0]){
-    		NewMns = NewMns + "PM";
-    	}
-    	else{
-    		NewMns = NewMns + "PM";
-    	}
-    }
-
-    console.log(hrs + ":" + mns);
-    return "Multiplied Exam Time: [ " + origMultHrs.toString().padStart(2, '0') +  ":" + origMultMns.toString().padStart(2, '0') +
-    " ] Exam Stop Time: [ "+ hrs.toString().padStart(2, '0') + ":" +  NewMns +" ]";
-  }
-
-  function fourF(orig, sTime){
-  	let flag = true; //true am
-    orig = orig.split(":");
-    sTime = sTime.split(":");
-    var origDate = new Date(0, 0, 0, orig[0], orig[1], 0, 0);
-    var startDate = new Date(0, 0, 0, sTime[0], sTime[1], 0, 0);
-
-    // minutes are worth 60 seconds. Hours are worth 60 minutes.
-    if(orig[0]>12){
-      orig[0] = orig[0]-12;//keeping within 12 hr format
-    }
-    if(sTime[0]>=12){
-    	if(sTime[0]<24){
-    		flag = false; //pm
-    	}
-    	else if(sTime[0]===12){
-    		flag=false;
-    	}
-    	else{
-    		flag = true; //am
-    	}
-      if(sTime[0]!=12)
-      {
-      	sTime[0] = sTime[0]-12;//keeping within 12 hr format if hr between 13 and 24 hrs
-      }
-    }
-
-    var seconds = orig[0] * 60 * 60 + orig[1] * 60;//original exam time in seconds
-    seconds = seconds* (3);//original exam time multiplied by multiplier
-
-    var origMultHrs = Math.floor((seconds/60)/60);
-    var origMultMns = Math.floor((seconds - ((origMultHrs*60)*60))/60);
-    var secb = sTime[0] * 60 * 60 + sTime[1] * 60;// start time
-    var res = seconds + secb;
-
-    var hrs = Math.floor((res/60)/60);
-    var mns = Math.floor((res - ((hrs*60)*60))/60);
-    if(hrs>12){
-      hrs = hrs-12;
-    }
-
-    var NewMns = mns.toString().padStart(2, '0');
-   if(flag){
-    	if((hrs=== 12 || hrs<sTime[0])&& sTime[0]!=12){
-    		NewMns = NewMns + "PM";
-    	}
-    	else if(hrs>sTime[0]){
-    		NewMns = NewMns + "AM";
-    	}
-    	else if(sTime[0]===12 && hrs<sTime[0]){
-    		NewMns = NewMns + "AM";
-    	}
-    	else{
-    		NewMns = NewMns + "AM";
-    	}
-    }
-    else if(!flag){
-    	if((hrs=== 12 || hrs<sTime[0])&& sTime[0]!=12){
-    		NewMns = NewMns + "AM";
-    	}
-    	else if(hrs>sTime[0]){
-    		NewMns = NewMns + "PM";
-    	}
-    	else if(sTime[0]===12 && hrs<sTime[0]){
-    		NewMns = NewMns + "PM";
-    	}
-    	else{
-    		NewMns = NewMns + "PM";
-    	}
-    }
-
-    console.log(hrs + ":" + mns);
-    return "Multiplied Exam Time: [ " + origMultHrs.toString().padStart(2, '0') +  ":" + origMultMns.toString().padStart(2, '0') +
-    " ] Exam Stop Time: [ "+ hrs.toString().padStart(2, '0') + ":" +  NewMns +" ]";
-  }
-
-
-
+ 
   function resetAll(){
     console.log("Reset Button Clicked!");
     var text = document.getElementById("input-text1");
